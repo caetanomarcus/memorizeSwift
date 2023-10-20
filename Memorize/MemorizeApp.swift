1c27447a1ed5bdb4dd8b4e08e1cb74e14fbce9a8
Marcus Caetano <caetanomarcusf@gmail.com>
2023-10-20 20:35:18 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Marcus Vinicius Caetano de Freitas on 20/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
