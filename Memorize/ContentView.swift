8d5a2c372a901c74bc342dbd83f46d5d457af9a4
Marcus Caetano <caetanomarcusf@gmail.com>
2023-10-21 10:40:16 -0300

    Finished RT1 Assingment 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,37 +9,73 @@ import SwiftUI
 
 struct ContentView: View {
     let emojis : [String] = ["😍", "😇", "😜", "😡"]
-    
+    @State var cardCount: Int = 4
     var body: some View {
-        HStack {
-            ForEach(emojis.indices, id: \.self) { index in
+        VStack {
+            ScrollView {
+                cards
+            }
+            Spacer()
+            cardCountAdjusters
+
+        }.padding()
+    }
+    
+    var cards: some View {
+        LazyVGrid (columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) { index in
                 CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
             }
         }
-        .padding()
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }.imageScale(.large)
+        .font(.largeTitle)
+
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardRemover: some View {
+        return cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
+    
+    var cardAdder: some View {
+        cardCountAdjuster(by: 1, symbol: "rectangle.stack.badge.plus.fill")
     }
 }
 
 struct CardView: View {
     let content: String
-    @State var isFaceUp: Bool = false
+    @State var isFaceUp: Bool = true
     
     var body: some View {
         ZStack{
             let base = RoundedRectangle(cornerRadius: 12.0)
-            if isFaceUp {
+            Group {
                 base.fill(.white)
                 base
                     .strokeBorder(lineWidth: 2.0)
                     .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
                 Text(content)
-            } else {
-                base
-                    .foregroundColor(.blue)
-            }
+            }.opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
         }.onTapGesture {
             isFaceUp.toggle()
-        }
+        }.foregroundColor(.orange)
     }
 }
 
